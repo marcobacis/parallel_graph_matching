994ca2bcb542238e0fe3d5556b332b031641c928
Antonio Di Bello <antonio.dibello@mail.polimi.it>
2017-12-24 00:44:47 +0100

    Update epsilon using heuristic for dense graph

--- match.cpp
+++ match.cpp
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 #include <cfloat>
 #include <vector>
 #include <unordered_set>
@@ -9,9 +10,9 @@
 
 using namespace std;
 
-int n=0; //TODO a cosa è uguale n ? Al numero di iterazioni ?
+int n=10; //TODO a cosa è uguale n ? Al numero di iterazioni ?
 
-void auction(int na, int nb, float *x){
+void auction(int na, int nb, float *x){ // na <= nb , na buyers, nb objects
     vector <int> match(na,-1);
     vector <int> assigned(nb,-1);
     unordered_set <int> freeBuyer; //TODO or class template specialization<vector>   std::vector<bool>
@@ -22,59 +23,70 @@ void auction(int na, int nb, float *x){
     float (*X)[nb] = (float (*)[nb]) x;
 
     /* Initialize epsilon */
-    int teta = 16;
-    int xi = 2;
+    float teta = 16;
+    float xi = 2;
     float epsilon = (n+1)/teta;
-    float deltaeps = 1/(n+1);
+    float deps = 1/(n+1);
     float gamma = (n+1)/teta;
+    float delta = floor(min(na/xi, n/teta));
 
     int bestObj;
-    int maxProfit;
-    int secondProfit;
+    float maxProfit;
+    float secondProfit;
     int buyer;
 
     while (!freeBuyer.empty()) {
-        /* Bidding */
-        maxProfit = FLT_MIN;
-        secondProfit = FLT_MIN;
-        bestObj = -1;
-        for (auto it = freeBuyer.begin(); it != freeBuyer.end(); it++) {
-            for (int j=0; j<nb; j++) {
-                if (X[*it][j] - price[j] > maxProfit) {
-                    if (j != bestObj) {
-                        secondProfit = maxProfit;
+        epsilon = teta/(n+1);
+        while (freeBuyer.size() > delta) {
+            /* Bidding */
+            maxProfit = FLT_MIN;
+            secondProfit = FLT_MIN;
+            bestObj = -1;
+            for (auto it = freeBuyer.begin(); it != freeBuyer.end(); it++) {
+                for (int j=0; j<nb; j++) {
+                    if (X[*it][j] - price[j] > maxProfit) {
+                        if (j != bestObj) {
+                            secondProfit = maxProfit;
+                        }
+                        bestObj = j;
+                        maxProfit = X[*it][j] - price[j];
+                        buyer = *it;
+                    } else if (j!=bestObj && X[*it][j] - price[j] > secondProfit) {
+                        secondProfit = X[*it][j] - price[j];
                     }
-                    bestObj = j;
-                    maxProfit = X[*it][j] - price[j];
-                    buyer = *it;
-                } else if (j!=bestObj && X[*it][j] - price[j] > secondProfit) {
-                    secondProfit = X[*it][j] - price[j];
                 }
             }
+
+            if (secondProfit == FLT_MIN)
+                secondProfit = 0;
+
+            /* Price update */
+            price[bestObj] += (maxProfit - secondProfit + epsilon);
+
+            /* Assignment */
+
+            /* delete previous match (if exists)
+               and insert the old owner in free buyer */
+            if (assigned[bestObj] != -1 && assigned[bestObj]!=buyer) {
+                match[assigned[bestObj]] = -1;
+                freeBuyer.insert(assigned[bestObj]);
+            }
+
+            /* make the new match */
+            match[buyer] = bestObj;
+            assigned[bestObj] = buyer;
+            freeBuyer.erase(buyer);
+
+            /* update epsilon */
+            if (gamma > epsilon) {
+                epsilon *= xi;
+            } else {
+                epsilon = gamma;
+            }
+            gamma /= xi;
         }
-
-        if (secondProfit == FLT_MIN)
-            secondProfit = 0;
-
-        /* Price update */
-        price[bestObj] += (maxProfit - secondProfit + epsilon);
-
-        /* Assignment */
-
-        /* delete previous match (if exists)
-           and insert the old owner in free buyer */
-        if (assigned[bestObj] != -1 && assigned[bestObj]!=buyer) {
-            match[assigned[bestObj]] = -1;
-            freeBuyer.insert(assigned[bestObj]);
-        }
-
-        /* make the new match */
-        match[buyer] = bestObj;
-        assigned[bestObj] = buyer;
-        freeBuyer.erase(buyer);
-
-        /* update epsilon */
-        epsilon = max(epsilon,epsilon-deltaeps);
+        delta /= xi;
+        teta *= xi;
     }
 
     for (int i=0;i<na;i++)
